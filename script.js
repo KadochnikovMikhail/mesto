58e875f63a6d86dd0b290649208cf1a190d2a0db
Mikhail Kadochnikov <mihaKaDAl2003@gmail.com>
2021-11-21 22:29:20 +0500

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+let profile = document.querySelector('.profile');
+let info = profile.querySelector('.profile__info');
+let editButton = info.querySelector('.profile__info_header_edit-button');
+
+let popup = document.querySelector('.popup');
+
+if (popup.addEventListener){
+    popup.className()
+}
