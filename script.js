dad4bec655ae0eec2ede65a13f85d3a523b944e7
Mikhail Kadochnikov <mihaKaDAl2003@gmail.com>
2021-11-21 23:36:47 +0500

    second

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ let info = profile.querySelector('.profile__info');
 let editButton = info.querySelector('.profile__info_header_edit-button');
 
 let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__closeButton');
 
-if (popup.addEventListener){
-    popup.className()
+function openPopup(){
+    popup.className = 'popup__opened';
 }
+editButton.addEventListener('click', openPopup);
+    
+function closePopup(){
+    popup.className = 'popup';
+}
+closeButton.addEventListener('click', closePopup);
+
+let buttonSave = popup.querySelector('.popup__buttonSave');
+
+buttonSave.addEventListener('submit', function () {
+    evt.preventDefault();
+    
+});
