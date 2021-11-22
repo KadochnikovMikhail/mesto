94be5e3c33f6c0229945d7201da18b40e7804d08
Mikhail Kadochnikov <mihaKaDAl2003@gmail.com>
2021-11-22 10:51:09 +0500

    third

--- README.md
+++
@@ -1,7 +0,0 @@
-# Проект: Путешествие по России
-
-[ссылка на проект](https://kadochnikovmikhail.github.io/mesto/)
-
-
-
-© 2021 Mikhail
--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let profile = document.querySelector('.profile');
 let info = profile.querySelector('.profile__info');
 let editButton = info.querySelector('.profile__info_header_edit-button');
-
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__closeButton');
+let name = document.querySelector('.profile__info_header_name');
+let job = document.querySelector('.profile__info_description');
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#job');
+let buttonSave = popup.querySelector('.popup__buttonSave');
+let formElement = popup.querySelector('.popup__conteiner');
 
 function openPopup(){
     popup.className = 'popup__opened';
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
 }
+
 editButton.addEventListener('click', openPopup);
     
 function closePopup(){
@@ -15,9 +23,14 @@ function closePopup(){
 }
 closeButton.addEventListener('click', closePopup);
 
-let buttonSave = popup.querySelector('.popup__buttonSave');
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    let name = document.querySelector('.profile__info_header_name');
+    let job = document.querySelector('.profile__info_description');
 
-buttonSave.addEventListener('submit', function () {
-    evt.preventDefault();
-    
-});
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+    closePopup()
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
