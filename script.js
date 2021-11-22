29908d9b1172fb4a2f0df0d7fa0037eb1cd1e092
Mikhail Kadochnikov <mihaKaDAl2003@gmail.com>
2021-11-22 13:31:28 +0500

    fourth fix

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-let profile = document.querySelector('.profile');
-let info = profile.querySelector('.profile__info');
-let editButton = info.querySelector('.profile__info_header_edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__closeButton');
-let name = document.querySelector('.profile__info_header_name');
+const profile = document.querySelector('.profile');
+const info = profile.querySelector('.profile__info');
+const editButton = info.querySelector('.profile__info_header_edit-button');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__closeButton');
+let nameProfile = document.querySelector('.profile__info_header_name');
 let job = document.querySelector('.profile__info_description');
 let nameInput = document.querySelector('#name');
 let jobInput = document.querySelector('#job');
-let buttonSave = popup.querySelector('.popup__buttonSave');
-let formElement = popup.querySelector('.popup__conteiner');
+const buttonSave = popup.querySelector('.popup__buttonSave');
+const formElement = popup.querySelector('.popup__container');
 
 function openPopup(){
     popup.className = 'popup__opened';
-    nameInput.value = name.textContent;
+    nameInput.value = nameProfile.textContent;
     jobInput.value = job.textContent;
 }
 
 editButton.addEventListener('click', openPopup);
     
 function closePopup(){
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = job.textContent;
     popup.className = 'popup';
 }
 closeButton.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    let name = document.querySelector('.profile__info_header_name');
-    let job = document.querySelector('.profile__info_description');
 
-    name.textContent = nameInput.value;
+    nameProfile.textContent = nameInput.value;
     job.textContent = jobInput.value;
     closePopup()
 }
