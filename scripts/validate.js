b2b74b86ad304a11ac8970905bc04e2492aca8e4
Mikhail Kadochnikov <mihaKaDAl2003@gmail.com>
2021-12-22 21:35:05 +0500

    6th project 1

---
+++ scripts/validate.js
@@ -0,0 +1,72 @@
+
+const showError = (form, input, errorMessageText, errorMessageClass, inputErrorClass) => {
+    const errorMessage = form.querySelector(`#${input.id}-error`)
+    errorMessage.textContent = errorMessageText
+    errorMessage.classList.add(errorMessageClass)
+    input.classList.add(inputErrorClass)
+}
+
+const hideError = (form, input, errorMessageClass, inputErrorClass) => {
+    const errorMessage = form.querySelector(`#${input.id}-error`)
+    errorMessage.textContent = ''
+    errorMessage.classList.remove(errorMessageClass)
+    input.classList.remove(inputErrorClass)
+}
+
+const hasInvalidInput = (inputs) => {
+    return Array.from(inputs).some((el) => !el.validity.valid)
+}
+
+const toggleButtonError = (inputs, button, inactiveButtonClass) => {
+    if (hasInvalidInput(inputs)) {
+        button.classList.add(inactiveButtonClass)
+        button.disabled = true
+    }
+    else {
+        button.classList.remove(inactiveButtonClass)
+        button.disabled = false
+    }
+}
+
+const checkIfInputValid = (form, input, { inputErrorClass, errorClass }) => {
+    if (!input.validity.valid) {
+        showError(form, input, input.validationMessage, errorClass, inputErrorClass)
+    }
+    else {
+        hideError(form, input, inputErrorClass, errorClass)
+    }
+}
+
+const setInputListners = (form, { inputSelector, inactiveButtonClass, ...rest }) => {
+    const inputs = form.querySelectorAll(inputSelector)
+    const submitButton = form.querySelector(submitButtonSelector)
+
+    inputs.forEach((input) => {
+        input.addEventListner('input', () => {
+            checkIfInputValid(form, input, rest)
+            toggleButtonError(inputs, submitButton, inactiveButtonClass )
+        })
+    })
+}
+
+
+const enableValidation = (formSelector, ...rest) => {
+    const forms = document.querySelectorAll(formSelector)
+
+    forms.forEach((form) => {
+        form.addEventListner('submit', (event) => {
+            event.preventDefault()
+        })
+
+        setInputListners(form, rest)
+    })
+}
+
+enableValidation({
+    formSelector: 'form',
+    inputSelector: 'form__data',
+    submitButtonSelector: 'form__buttonsave',
+    inactiveButtonClass: 'form__buttonsave_disabled',
+    inputErrorClass: 'form__data_type_error',
+    errorClass: 'popup__error_visible'
+});
